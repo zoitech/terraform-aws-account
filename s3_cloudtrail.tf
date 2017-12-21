1b92764a727985491f588a85c43189c5f62485dc
Sebastian Melchior <sebastian@melchior.me>
2017-12-21 09:38:37 +0100

    encrypt cloudtrail bucket by default

--- s3_cloudtrail.tf
+++ s3_cloudtrail.tf
@@ -2,6 +2,15 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
   bucket = "${local.bucket_name}"
   acl    = "private"
   count  = "${var.trail_bucketname_create}"
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${var.trail_bucket_default_encryption_key}"
+        sse_algorithm     = "${var.trail_bucket_default_encryption}"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
