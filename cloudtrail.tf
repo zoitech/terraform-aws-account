1ccf66ccb03089353f1be07aeeed8d4a9b47f664
Malte Brodersen <malte.brodersen@zoi.de>
2018-06-12 15:17:29 +0200

    Fix issue for non working dependency of policy and bucket. Fix usage of external bucket.

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,7 +1,7 @@
 # CloudTrail
 resource "aws_cloudtrail" "global_Default" {
   name                          = "${var.trail_name}"
-  s3_bucket_name                = "${aws_s3_bucket.cloudtrail_bucket.id}"
+  s3_bucket_name                = "${local.bucket_name}"
   is_multi_region_trail         = true
   include_global_service_events = true
   enable_log_file_validation    = true
